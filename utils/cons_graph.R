94cd15897005f51cb1293dc4e0c92b54db6285c2
taerim <qwert12a@ajou.ac.kr>
2018-05-04 18:14:58 +0900

    construction of directed graph

--- utils/cons_graph.R
+++ utils/cons_graph.R
@@ -1,114 +1,71 @@
-
-
-datapath <- file.path('data')
-kegg_db <- '../data/KEGG_DB/'
-##################################################################################
-##################################################################################
-#############Constructing Directed Gene-gene GRAPH################################
-
-# This code implements constructing global directed gene-gene graph from all kegg pathway(525 pathways)
-#525 pathways -> 327 pathways(It is the number of existing kgml)
-
-if(!dir.exists(kegg_db)) {
-  kegg_list <- scan("../data/kegg_pathway", what="", sep = "\n")
-  kgml_list <- list()
+cons_graph <- function(datapath){
   
-  for(pathid in kegg_list){
-    tryCatch(
-      {
-        tmp <- paste('../data/KEGG_DB/hsa',pathid,sep="")
-        tmp <- paste(tmp,".xml", sep="")
-        retrieveKGML(pathwayid = pathid, organism = "hsa", destfile = tmp, method = "wget")
-        kgml_list <- append(kgml_list, tmp)
-        cat(pathid,'is downloaded!...................')
+  # This code implements constructing global directed gene-gene graph from all kegg pathway(525 pathways)
+  # 525 pathways -> 327 pathways(It is the number of existing kgml)
+  # directGraph
+  # node : 7389
+  # edge : 58399 
+  
+  datapath <- file.path('data')
+  kegg_db <- file.path(datapath,'KEGG_DB')
+  
+  if(!dir.exists(kegg_db)) {
+    cat("kgml file doesn't exist.")
+    kegg_list <- scan("../data/kegg_pathway", what="", sep = "\n")
+    kgml_list <- list()
+    
+    for(pathid in kegg_list){
+      tryCatch(
+        {
+          tmp <- paste('../data/KEGG_DB/hsa',pathid,sep="")
+          tmp <- paste(tmp,".xml", sep="")
+          retrieveKGML(pathwayid = pathid, organism = "hsa", destfile = tmp, method = "wget")
+          kgml_list <- append(kgml_list, tmp)
+          cat(pathid,'is downloaded!...................')
+          
+        },
+        error=function(e){
+          cat(pathid,'is not exist')
+          message(e)
+        }
         
-      },
-      error=function(e){
-        cat(pathid,'is not exist')
-        message(e)
-      }
-      
-    )
+      )
+    }
+    
+  } else{
+    cat("kgml files already exists")
+    kgml_list <- list.files(path=kegg_db, full.names=TRUE)
   }
   
-} else{
   
-  kgml_list <- list.files(path='../data/KEGG_DB', full.names=TRUE)
+  graphs <- list()
+  pathSet <- list()
+  
+  for(kgml in kgml_list){
+    
+    # parse kgml file
+    k <- parseKGML(kgml)
+    
+    # pathway set
+    node_names <- unlist(lapply(X=nodes(k),
+                                FUN=function(x) {
+                                  ifelse(getType(x)=="gene", getSYMBOL(sapply( strsplit(getName(x),":"), '[[', 2 ), data='org.Hs.eg'), NA)
+                                }), use.names = F)
+    pathSet[[substring(getName(k),9)]] <- node_names[!is.na(node_names)]
+    
+    # graph set
+    pathwayG <- parseKGML2Graph(kgml, expandGenes=TRUE)
+    graphs <- append(graphs, pathwayG)
+  }
+  
+  # make direct graph
+  merged_G <- mergeGraphs(graphs)
+  directGraph <- igraph.from.graphNEL(merged_G)
+  
+  V(directGraph)$name <- sapply(X = V(directGraph)$name, FUN = function(x) getSYMBOL(strsplit(x, ":")[[1]][2], data='org.Hs.eg'), USE.NAMES = F)
+  
+  save(directGraph, file=file.path(datapath, paste(c("directGraph", "rda"), collapse='.')))
+  save(pathSet, file=file.path(datapath, paste(c("pathSet", "rda"), collapse='.')))
+
 }
-
-
-#kgml_list <- paste('../data/KEGG_DB/',exist_list,sep="")
-graphs <- list()
-pathSet <- list()
-
-for(kgml in kgml_list){
-  
-  # parse kgml file
-  k <- parseKGML(kgml)
-  
-  # pathway set
-  node_names <- unlist(lapply(X=nodes(k),
-                              FUN=function(x) {
-                                ifelse(getType(x)=="gene", getSYMBOL(sapply( strsplit(getName(x),":"), '[[', 2 ), data='org.Hs.eg'), NA)
-                              }), use.names = F)
-  pathSet[[substring(getName(k),9)]] <- node_names[!is.na(node_names)]
-  
-  # graph set
-  pathwayG <- parseKGML2Graph(kgml, expandGenes=TRUE)
-  graphs <- append(graphs, pathwayG)
-}
-
-
-merged_G <- mergeGraphs(graphs)
-directGraph <- igraph.from.graphNEL(merged_G)
-
-V(directGraph)$name <- sapply(X = V(directGraph)$name, FUN = function(x) getSYMBOL(strsplit(x, ":")[[1]][2], data='org.Hs.eg'), USE.NAMES = F)
-
-save(directGraph, file=file.path(datapath, paste(c("directGraph", from, "rda"), collapse='.')))
-save(pathSet, file=file.path(datapath, paste(c("pathSet", from, "rda"), collapse='.')))
-
-##################################################################################
-##################################################################################
-#########################Constructing PPI GRAPH###################################
-#pathway : 1546602 * 2
-#rppa : 188 * 376
-pathway <- read.csv(file="~/protein-integration/data/PathwayCommons9.All.hgnc.sif", sep="\t", header = FALSE)
-rppa <- read.csv(file="~/protein-integration/data/mean_imputed_rppa.csv", header=TRUE, row.names = 1)
-substring(rownames(rppa),1,1)
-row.names(rppa) <- substring(rownames(rppa),2)
-
-pathway[2] <- NULL
-genepair <- unique(pathway)
-adjmtx <- get.adjacency(graph.edgelist(as.matrix(genepair), directed=FALSE))
-ppi <- graph_from_adjacency_matrix(adjmtx, mode = "undirected")
-
-#ppi genes : 31693
-#ppi edges : 1461050 (regardless of interaction type) 
-gsize(ppi)
-
-#plot the degree of the igraph(X:gene, Y:edge)
-#number of the Node degree > 4000 => 25
-########LIST##########
-#"APP"         "MYC"         "SP1"         "TCF3"        "CHEBI:16469" "CHEBI:23965" "CHEBI:15367"
-#"CHEBI:26536" "CHEBI:33364" "NOG"         "CHEBI:39867" "CHEBI:46195" "CHEBI:23414" "CHEBI:2504" 
-#"CHEBI:27899" "CHEBI:29865" "CHEBI:30563" "CHEBI:31440" "CHEBI:4031"  "CHEBI:46024" "CHEBI:4667" 
-#"CHEBI:60654" "CHEBI:78510" "CHEBI:91108" "CHEBI:9925" 
-######################
-ppi.degrees <- degree(ppi)
-ppi_df <- as.data.frame(ppi.degrees)
-hist(ppi_df$ppi.degrees, breaks = 1000, xlab = "Degree", main = "Degree-Frequency histogram in PPI")
-ppi_log10 <- log10(ppi_df$ppi.degrees)
-hist(ppi_log10, breaks = 1000, xlab = "Degree", main = "Degree-Frequency log10-histogram in PPI")
-#hub_gene <- ppi.degrees[ppi.degrees>4000]
-#barplot(hub_gene, names.arg = ppi.degrees, cex.names=0.7)
-chebi <- grep("CHEBI", rownames(ppi_df))
-ppi_without_chebi <- ppi_df[-chebi,1]
-hist(ppi_without_chebi, breaks = 1000, xlab = "Degree", main = "Degree-Frequency histogram in PPI without CHEBI")
-ppi_without_chebi_log10 <- log10(ppi_without_chebi)
-hist(ppi_without_chebi_log10, breaks = 1000, xlab = "Degree", main = "Degree-Frequency log10-histogram in PPI without CHEBI")
-
-#intersection of ppi and rppa : 186
-ppi_gene <- V(ppi)
-rppa_gene <- rownames(rppa)
-common_gene <- intersect(names(ppi_gene),rppa_gene)
 
