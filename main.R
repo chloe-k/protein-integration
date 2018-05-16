013e1e2f2309762dcabe59dbcde9edb88eca9def
taerim <qwert12a@ajou.ac.kr>
2018-05-16 17:03:43 +0900

    commit

--- main.R
+++ main.R
@@ -1,12 +1,12 @@
 source("https://bioconductor.org/biocLite.R")
+
 library(KEGGgraph)
 library(igraph)
 library(ggplot2)
 library(annotate)
 library(org.Hs.eg.db)
-library(RANKS)
+library(diffusr)
 library(Matrix)
-library(NetPreProc)
 
 sapply(file.path("utils",list.files("utils", pattern="*.R")),source)
 
@@ -14,13 +14,15 @@ sapply(file.path("utils",list.files("utils", pattern="*.R")),source)
 datapath <- file.path('data')
 if(!dir.exists(datapath)) dir.create(datapath)
 
+gdacpath <- file.path(datapath, 'BRCA_GDAC')
+
 respath <- file.path('result')
 if(!dir.exists(respath)) dir.create(respath)
 
 # read rda data
 graphpath <- file.path(datapath,'directGraph.rda')
 if(!file.exists(graphpath)) {
-  cat('directGraph and pathSet do not exist, now creating directGraph and pathSet is start')
+  print('directGraph and pathSet do not exist, now creating directGraph and pathSet is start')
   cons_graph(datapath)
 }
 load(file.path(graphpath))
@@ -31,9 +33,8 @@ load(file.path(datapath, 'pathSet.rda'))
 rppapath <- file.path(datapath, 'mean_imputed_rppa.csv')
 
 if(!file.exists(rppapath)) {
-  cat('preprocessed rppa profile does not exist, now preprocessing RPPA profile start')
-  rawrppapath <- file.path(datapath, 'brca_rppa.txt')
-  preprcs_rppa(datapath, rawrppapath)
+  print('preprocessed rppa profile does not exist, now preprocessing RPPA profile start')
+  preprcs_rppa(gdacpath)
 }
 
 #w0_rppa <- imput_ppi(datapath, rppapath)
@@ -41,8 +42,8 @@ if(!file.exists(rppapath)) {
 ppipath <- file.path(datapath, 'ppiGraph.rda')
 dppipath <- file.path(datapath, 'DppiGraph.rda')
 if(!file.exists(ppipath)) {
-  cat('ppiGraph does not exist, now creating ppiGraph start')
-  cons_ppi(datapath)
+  print('ppiGraph does not exist, now creating ppiGraph start')
+  cons_ppi(datapath, gdacpath)
 }
 load(file.path(ppipath))
 load(file.path(dppipath))
@@ -53,4 +54,16 @@ m <- directGraph
 dp <- DppiGraph
 p <- ppiGraph
 
-imput_ppi(datapath,rppapath,p)
+dfsppi <- 'diffus_ppi_8rel'
+dfsppipath <- file.path(datapath, paste(c(dfsppi,'rda'), collapse = '.'))
+if(!file.exists(dfsppipath)){
+  print('diffused PPI with RPPA')
+  diffus_ppi(datapath, gdacpath, p, dfsppi)
+}
+load(file.path(dfsppipath))
+
+
+
+
+
+
