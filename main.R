c67f21181b44fce55ed7acad011c17bfee434cb9
taerim <qwert12a@ajou.ac.kr>
2018-07-18 20:17:05 +0900

    result 18_all

--- main.R
+++ main.R
@@ -97,16 +97,16 @@ y=list(good_samples, poor_samples)
 
 
 ################################################### Result25  #################################################
-res_models <- list()
+res_models2 <- list()
 #------------------------- RNAseq + Methyl + RPPA(Pathway Graph) -------------------------#
 gmr <- g %du% m %du% r
 testStatistic <- c("DESeq2", "t-test", "t-test")
 profile_name <- c("rna(Entrez)", "meth(Entrez)", "rppa(Pathway_Graph_Entrez)")
 x=list(rnaseq, imputed_methyl, rppa)
 
-fit.iDRWPClass(x=x, y=y, globalGraph=gmr, testStatistic= testStatistic, profile_name = profile_name,
-               datapath = datapath, respath = respath, pathSet=pathSet, method = "DRW", samples = samples,
-               id = "result25_GMR", prob = 0.4, Gamma = 0.2, pranking = "t-test", mode = "GMR", AntiCorr=FALSE, DEBUG=TRUE)
+# fit.iDRWPClass(x=x, y=y, globalGraph=gmr, testStatistic= testStatistic, profile_name = profile_name,
+#                datapath = datapath, respath = respath, pathSet=pathSet, method = "DRW", samples = samples,
+#                id = "result25_GMR", prob = 0.4, Gamma = 0.2, pranking = "t-test", mode = "GMR", AntiCorr=FALSE, DEBUG=TRUE)
 
 res_pa_GMR_25 <- fit.classification(y=y, samples = samples, id = "result25_GMR", datapath = datapath, respath = respath,
                                       profile_name = profile_name, method = "DRW", pranking = "t-test", classifier = "rf",
@@ -114,7 +114,7 @@ res_pa_GMR_25 <- fit.classification(y=y, samples = samples, id = "result25_GMR",
 
 save(res_pa_GMR_25, file=file.path('data/model/res_pa_GMR_25.RData'))
 
-res_models <- c(res_models, list(res_pa_GMR_25))
+res_models2 <- c(res_models2, list(res_pa_GMR_25))
 
 
 #------------------------- RNAseq + Methyl + RPPA(diffused Pathway Graph) -------------------------#
@@ -123,9 +123,9 @@ testStatistic <- c("DESeq2", "t-test", "t-test")
 profile_name <- c("rna(Entrez)", "meth(Entrez)", "rppa(diffused_Pathway_Graph_Entrez)")
 x=list(rnaseq, imputed_methyl, rppa)
 
-fit.iDRWPClass(x=x, y=y, globalGraph=gmr, testStatistic= testStatistic, profile_name = profile_name,
-               datapath = datapath, respath = respath, pathSet=pathSet, method = "DRW", samples = samples,
-               id = "result25_GMR_d", prob = 0.4, Gamma = 0.2, pranking = "t-test", mode = "GMR_d", AntiCorr=FALSE, DEBUG=TRUE)
+# fit.iDRWPClass(x=x, y=y, globalGraph=gmr, testStatistic= testStatistic, profile_name = profile_name,
+#                datapath = datapath, respath = respath, pathSet=pathSet, method = "DRW", samples = samples,
+#                id = "result25_GMR_d", prob = 0.4, Gamma = 0.2, pranking = "t-test", mode = "GMR_d", AntiCorr=FALSE, DEBUG=TRUE)
 
 res_pa_GMR_d_25 <- fit.classification(y=y, samples = samples, id = "result25_GMR_d", datapath = datapath, respath = respath,
                                          profile_name = profile_name, method = "DRW", pranking = "t-test", classifier = "rf",
@@ -133,7 +133,7 @@ res_pa_GMR_d_25 <- fit.classification(y=y, samples = samples, id = "result25_GMR
 
 save(res_pa_GMR_d_25, file=file.path('data/model/res_pa_GMR_d_25.RData'))
 
-res_models <- c(res_models, list(res_pa_GMR_d_25))
+res_models2 <- c(res_models2, list(res_pa_GMR_d_25))
 
 
 
@@ -141,9 +141,9 @@ res_models <- c(res_models, list(res_pa_GMR_d_25))
 # plot
 
 # GM
-title <- c("Result 25")
+title <- c("Result 25(10 intersect genes)")
 xlabs <- c("GMR", "GMR_d")
 
-perf_min <- min(sapply(X = res_models, FUN = function(x){mean(x$results$Accuracy)}))
-perf_max <- max(sapply(X = res_models, FUN = function(x){mean(x$results$Accuracy)}))
-perf_boxplot(title, xlabs, res_models, perf_min = perf_min-0.02, perf_max = perf_max+0.02)
+perf_min <- min(sapply(X = res_models2, FUN = function(x){x$results$Accuracy}))
+perf_max <- max(sapply(X = res_models2, FUN = function(x){x$results$Accuracy}))
+perf_boxplot(title, xlabs, res_models2, perf_min = perf_min-0.02, perf_max = perf_max+0.02)
--- utils/getPathActivity.R
+++ utils/getPathActivity.R
@@ -14,10 +14,11 @@ getPathActivity <-
       for (i in 1 : length(pathSet)){
         Vpathwayi <- pathSet[[i]]
         ##############################################################################
-        com_rna <- which(substring(rownames(x),1,1) == 'g' & substring(rownames(x),2) %in% Vpathwayi)
-        com_met <- which(substring(rownames(x),1,1) == 'm' & substring(rownames(x),2) %in% Vpathwayi)
-        com_pro <- which(substring(rownames(x),1,1) == 'p' & substring(rownames(x),2) %in% Vpathwayi)
-        flag <- length(com_rna)>=5 & length(com_met)>=5 & length(com_pro)>=5
+        flag <- TRUE
+        # com_rna <- which(substring(rownames(x),1,1) == 'g' & substring(rownames(x),2) %in% Vpathwayi)
+        # com_met <- which(substring(rownames(x),1,1) == 'm' & substring(rownames(x),2) %in% Vpathwayi)
+        # com_pro <- which(substring(rownames(x),1,1) == 'p' & substring(rownames(x),2) %in% Vpathwayi)
+        # flag <- length(com_rna)>=10 & length(com_met)>=10 & length(com_pro)>=10
         ##############################################################################
         if (length(Vpathwayi) > 0 & flag){
           n <- 0    # the number of differential genes in ith pathway 			
