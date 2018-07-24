05908d00d9e0312823620520bccebdede07e0ac6
taerim <qwert12a@ajou.ac.kr>
2018-07-24 20:57:07 +0900

    commit by 222(gpu6)

--- utils/getPathActivity.R
+++ utils/getPathActivity.R
@@ -8,19 +8,23 @@ getPathActivity <-
     names(sigGenes) <- names(pathSet)
     TValue.pathActivity <- matrix(NA, nrow = length(pathSet), ncol = 1)
     rownames(TValue.pathActivity) <- names(pathSet)
-    
+    cnt_ovw <- 0
     if(method == "DRW") { 
       # vertex is weighted with DRW method
       for (i in 1 : length(pathSet)){
         Vpathwayi <- pathSet[[i]]
         ##############################################################################
         flag <- TRUE
+        # prot <- get(load('data/t-test.rppa(Entrez).RData')) # for result25_GM
         # com_rna <- which(substring(rownames(x),1,1) == 'g' & substring(rownames(x),2) %in% Vpathwayi)
         # com_met <- which(substring(rownames(x),1,1) == 'm' & substring(rownames(x),2) %in% Vpathwayi)
-        # com_pro <- which(substring(rownames(x),1,1) == 'p' & substring(rownames(x),2) %in% Vpathwayi)
+        # # com_pro <- which(substring(rownames(x),1,1) == 'p' & substring(rownames(x),2) %in% Vpathwayi)
+        # com_pro <- which(substring(rownames(prot),1,1) == 'p' & substring(rownames(prot),2) %in% Vpathwayi)
         # flag <- length(com_rna)>=lim & length(com_met)>=lim & length(com_pro)>=lim
         ##############################################################################
         if (length(Vpathwayi) > 0 & flag){
+          cnt_ovw <- cnt_ovw+1
+          # print(paste(c('g:',length(com_rna), 'm:',length(com_met), 'p:',length(com_pro)), collapse = ' '))
           n <- 0    # the number of differential genes in ith pathway 			
           pathActivity_tmp <- matrix(nrow=1,ncol=dim(x)[2],data=0) 
           TValue.pathActivity_tmp <- 0
@@ -29,15 +33,15 @@ getPathActivity <-
           
           for (j in 1 : length(Vpathwayi)){
             ########################### Result 23 ##################################
-            if(type_used == 'g') {Idx <- which(substring(rownames(x),1,1) == 'g' & substring(rownames(x),2)==Vpathwayi[j])}
-            else if(type_used == 'm') {Idx <- which(substring(rownames(x),1,1) == 'm' & substring(rownames(x),2)==Vpathwayi[j])}
-            else if(type_used == 'p') {Idx <- which(substring(rownames(x),1,1) == 'p' & substring(rownames(x),2)==Vpathwayi[j])}
-            else if(type_used == 'gm') {Idx <- which((substring(rownames(x),1,1) == 'g' | substring(rownames(x),1,1) == 'm') & substring(rownames(x),2)==Vpathwayi[j])}
-            else if(type_used == 'mp') {Idx <- which((substring(rownames(x),1,1) == 'm' | substring(rownames(x),1,1) == 'p') & substring(rownames(x),2)==Vpathwayi[j])}
-            else if(type_used == 'gp') {Idx <- which((substring(rownames(x),1,1) == 'g' | substring(rownames(x),1,1) == 'p') & substring(rownames(x),2)==Vpathwayi[j])}
-            else {Idx <- which(substring(rownames(x),2)==Vpathwayi[j])}
+            # if(type_used == 'g') {Idx <- which(substring(rownames(x),1,1) == 'g' & substring(rownames(x),2)==Vpathwayi[j])}
+            # else if(type_used == 'm') {Idx <- which(substring(rownames(x),1,1) == 'm' & substring(rownames(x),2)==Vpathwayi[j])}
+            # else if(type_used == 'p') {Idx <- which(substring(rownames(x),1,1) == 'p' & substring(rownames(x),2)==Vpathwayi[j])}
+            # else if(type_used == 'gm') {Idx <- which((substring(rownames(x),1,1) == 'g' | substring(rownames(x),1,1) == 'm') & substring(rownames(x),2)==Vpathwayi[j])}
+            # else if(type_used == 'mp') {Idx <- which((substring(rownames(x),1,1) == 'm' | substring(rownames(x),1,1) == 'p') & substring(rownames(x),2)==Vpathwayi[j])}
+            # else if(type_used == 'gp') {Idx <- which((substring(rownames(x),1,1) == 'g' | substring(rownames(x),1,1) == 'p') & substring(rownames(x),2)==Vpathwayi[j])}
+            # else {Idx <- which(substring(rownames(x),2)==Vpathwayi[j])}
             #########################################################################
-            # Idx <- which(substring(rownames(x),2)==Vpathwayi[j])
+            Idx <- which(substring(rownames(x),2)==Vpathwayi[j])
             # Idx <- which(substring(rownames(x),1,1) == 'm' & substring(rownames(x),2)==Vpathwayi[j])
             # Idx <- which((substring(rownames(x),1,1) == 'm' | substring(rownames(x),1,1) == 'p') & substring(rownames(x),2)==Vpathwayi[j])
             print(rownames(x)[Idx])
@@ -109,7 +113,7 @@ getPathActivity <-
     }
     
     colnames(pathActivity) <- colnames(x)
-    
+    print(cnt_ovw)
     # save pathway profile 
     write.table(x=t(pathActivity), file=fname, sep="\t", row.names=rows, col.names=T)
     cat('Pathway activity inference is done\n')
