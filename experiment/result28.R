d365729a524a33fd7a30f8f3db39b208e6898cc9
taerim <qwert12a@ajou.ac.kr>
2018-07-26 17:55:22 +0900

    result28:Gamma tuning in GR model

---
+++ experiment/result28.R
@@ -0,0 +1,16 @@
+################################## Result 28 in GR ############################################################
+registerDoParallel(cores = 4)
+
+# make RData after DRW
+
+id_list <- c("28_0.2", "28_0.4", "28_0.6", "28_0.8", "28_0.9")
+Gamma_list <- c(0.2, 0.4, 0.6, 0.8, 0.9)
+
+
+pack <- c("KEGGgraph", "igraph", "ggplot2", "annotate", "annotate", "org.Hs.eg.db", "diffusr", "DESeq2", "Matrix",
+          "stringr", "caret", "e1071", "randomForest", "KEGG.db", "KEGGREST")
+
+
+res_gmr_28 <- foreach(i=1:length(id_list), .packages = pack) %dopar%{
+  make_GR_model(id=id_list[i], prob = 0.001, Gamma = Gamma_list[i])
+}
