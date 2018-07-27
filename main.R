eaa91befa11d7831035c296586d214b77b38feb7
taerim <qwert12a@ajou.ac.kr>
2018-07-27 22:46:14 +0900

    commit

--- main.R
+++ main.R
@@ -95,19 +95,30 @@ y=list(good_samples, poor_samples)
 
 #----------------------------------------iDRW-----------------------------------------------------------#
 
+gmr <- g %du% m %du% r
+testStatistic <- c("DESeq2", "t-test", "t-test")
+profile_name <- c("rna(Entrez)", "meth(Entrez)", "rppa(Pathway_Graph_Entrez)")
+x=list(rnaseq, imputed_methyl, rppa)
+
+fit.iDRWPClass(x=x, y=y, globalGraph=gmr, testStatistic= testStatistic, profile_name = profile_name,
+               datapath = datapath, respath = respath, pathSet=pathSet, method = "DRW", samples = samples,
+               id = "result18_1_GMR", prob = 0.001, Gamma = 0, pranking = "t-test", mode = "GMR", AntiCorr=FALSE, DEBUG=TRUE)
+
+
+
 ################################## Result 28 in GR ############################################################
-registerDoParallel(cores = 4)
+registerDoParallel(cores = 5)
 
 # make RData after DRW
 
 id_list <- c("28_0.2", "28_0.4", "28_0.6", "28_0.8", "28_0.9")
 Gamma_list <- c(0.2, 0.4, 0.6, 0.8, 0.9)
-
+make_GR_model(id=id_list[1], prob = 0.001, Gamma = Gamma_list[1])
 pack <- c("KEGGgraph", "igraph", "ggplot2", "annotate", "annotate", "org.Hs.eg.db", "diffusr", "DESeq2", "Matrix",
           "stringr", "caret", "e1071", "randomForest", "KEGG.db", "KEGGREST")
 
 
-res_gr_28 <- foreach(i=2:length(id_list), .packages = pack) %dopar%{
+res_gr_28 <- foreach(i=1:length(id_list), .packages = pack) %dopar%{
   make_GR_model(id=id_list[i], prob = 0.001, Gamma = Gamma_list[i])
 }
 
@@ -115,17 +126,17 @@ res_gr_28 <- foreach(i=2:length(id_list), .packages = pack) %dopar%{
 
 # write sigFeature
 for(i in 1:length(id_list)){
-  load(paste(c('data/model/res_pa_GR_', id_list[i], '.RData'), collapse = ''))
+  load(paste(c('data/model/res_pa_GR_', id_list[i], '_LOOCV.RData'), collapse = ''))
 }
 
-res_gr_28 <- list(res_pa_GR_28_0.2, res_pa_GR_28_0.4, res_pa_GR_28_0.6,
-                  res_pa_GR_28_0.8, res_pa_GR_28_0.9)
+res_gr_28 <- list(res_pa_GR_28_0.2_LOOCV, res_pa_GR_28_0.4_LOOCV, res_pa_GR_28_0.6_LOOCV,
+                  res_pa_GR_28_0.8_LOOCV, res_pa_GR_28_0.9_LOOCV)
 
-profile_name <- c("rna(Entrez)", "rppa(Entrez)")
-for(i in 1:length(id_list)){
-  result_name <- paste(c('result',id_list[i],'_GR'), collapse = '')
-  write.SigFeatures(res_fit=res_gr_28[[i]], id = result_name, profile_name=profile_name, method="DRW", respath=respath)
-}
+# profile_name <- c("rna(Entrez)", "rppa(Entrez)")
+# for(i in 1:length(id_list)){
+#   result_name <- paste(c('result',id_list[i],'_GR'), collapse = '')
+#   write.SigFeatures(res_fit=res_gr_28[[i]], id = result_name, profile_name=profile_name, method="DRW", respath=respath)
+# }
 
 #GMR
 title <- c("Result 28 GR")
@@ -133,5 +144,5 @@ xlabs <- c("0.2", "0.4", "0.6", "0.8", "0.9")
 
 perf_min <- min(sapply(X = res_gr_28, FUN = function(x){max(x$results$Accuracy)}))
 perf_max <- max(sapply(X = res_gr_28, FUN = function(x){max(x$results$Accuracy)}))
-perf_boxplot(title, xlabs, res_gr_28, perf_min = perf_min-0.1, perf_max = perf_max+0.1)
+perf_boxplot(title, xlabs, res_gr_28, perf_min = perf_min-0.05, perf_max = perf_max+0.05)
 # perf_lineplot(title, xlabs, res_gr_28, perf_min=55, perf_max=95)
