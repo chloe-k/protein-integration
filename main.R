17683325e190901f9a148c42d92557b0abac0a25
taerim <qwert12a@ajou.ac.kr>
2018-07-28 20:29:15 +0900

    commit

--- main.R
+++ main.R
@@ -99,32 +99,37 @@ y=list(good_samples, poor_samples)
 
 #----------------------------------------iDRW-----------------------------------------------------------#
 
-################################## Result 31 in GMR ############################################################
+################################## Result 33 in GMR ############################################################
 
-num_cores <- detectCores()/2
 registerDoParallel(cores = 8)
 
-id_list <- c("31_1", "31_2", "31_3", "31_4", "31_5", "31_6", "31_7")
-type_list <- c("g", "m", "p", "gm", "gp", "mp", "gmp")
+id_list <- c("33_1", "33_2", "33_3")
+Gamma_list <- c(0.4, 0.6, 0.8)
 
-make_GMR_model(id=id_list[1], type_used = type_list[1], prob = 0.001, Gamma = 0.6)
+make_GMR_model(id=id_list[1], prob = 0.001, Gamma = Gamma_list[1], mode = "GMR_2", AntiCorr = TRUE)
 pack <- c("KEGGgraph", "igraph", "ggplot2", "annotate", "annotate", "org.Hs.eg.db", "diffusr", "DESeq2", "Matrix",
           "stringr", "caret", "e1071", "randomForest", "KEGG.db", "KEGGREST")
 
-res_gmr_31 <- foreach(i=2:length(id_list), .packages = pack) %dopar%{
-  make_GMR_model(id=id_list[i], type_used = type_list[i], prob = 0.001, Gamma = 0.6)
+res_gmr_33 <- foreach(i=1:length(id_list), .packages = pack) %dopar%{
+  make_GMR_model(id=id_list[i], prob = 0.001, Gamma = Gamma_list[i], mode = "GMR_2", AntiCorr = TRUE)
 }
 
-
+res_models <- list()
 for(i in 1:length(id_list)){
-  load(paste(c('data/model/res_pa_GMR_31_', i, '_LOOCV.RData'), collapse = ''))
+  model <- get(load(paste(c('data/model/res_pa_GMR_33_', i, '_LOOCV.RData'), collapse = '')))
+  res_models <- c(res_models, list(model))
 }
 
-res_gmr <- list(res_pa_GMR_31_1_LOOCV, res_pa_GMR_31_2_LOOCV, res_pa_GMR_31_3_LOOCV, res_pa_GMR_31_4_LOOCV, 
-                res_pa_GMR_31_5_LOOCV, res_pa_GMR_31_6_LOOCV, res_pa_GMR_31_7_LOOCV)
 
 for(i in 1:length(id_list)){
   result_name <- paste(c('result',id_list[i],'_GMR'), collapse = '')
   write.SigFeatures(res_fit=res_gmr[[i]], id = result_name, profile_name=profile_name, method="DRW", respath=respath)
 }
 
+# Plot for GMR model
+title <- c("Result 33_GMR")
+xlabs <- c("g=0.4", "g=0.6", "g=0.8")
+
+perf_min <- min(sapply(X = res_models, FUN = function(x){max(x$results$Accuracy)}))
+perf_max <- max(sapply(X = res_models, FUN = function(x){max(x$results$Accuracy)}))
+perf_boxplot(title, xlabs, res_models, perf_min = perf_min-0.02, perf_max = perf_max+0.02)
